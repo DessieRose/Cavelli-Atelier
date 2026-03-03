0f9539aa22fd2f3af1afdb2b4f60786563b4806c
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-03 14:12:26 +0100

    Add Material model, migrations, factory & seeder

---
+++ database/seeders/MaterialSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Material;
+
+class MaterialSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $materials = [
+            'Oak', 'Walnut', 'Pine', 'Steel',
+            'Glass', 'Velvet', 'Linen', 'Leather', 'Rattan', 'Marble',
+        ];
+
+        foreach ($materials as $name) {
+            Material::create(['name' => $name]);
+        }
+    }
+}
