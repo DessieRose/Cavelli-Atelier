e4e879daa4fd664b2ccaea88e4b52ec219c4cd50
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-05 15:25:02 +0100

    Add product edit/update/delete routes

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
     
+    
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
     Route::get('/colors', [ColorController::class, 'index'])->name('colors.index');
     Route::get('/colors/create', [ColorController::class, 'create'])->name('colors.create');
     Route::post('/colors', [ColorController::class, 'store'])->name('colors.store');
     Route::get('/colors/{color}/edit', [ColorController::class, 'edit'])->name('colors.edit');
     Route::put('/colors/{color}', [ColorController::class, 'update'])->name('colors.update');
     Route::delete('/colors/{color}', [ColorController::class, 'destroy'])->name('colors.destroy');
-    
+
     Route::get('/materials', [MaterialController::class, 'index'])->name('materials.index');
     Route::get('/materials/create', [MaterialController::class, 'create'])->name('materials.create');
     Route::post('/materials', [MaterialController::class, 'store'])->name('materials.store');
