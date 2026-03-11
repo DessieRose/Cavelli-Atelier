8c2a11a6d5baecb55c8bb1ce272732953c8b42f9
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-11 16:24:02 +0100

    Seed colors using Color::create instead of insert
    Replace bulk insert with an associative array + foreach that calls Color::create for each color. This ensures model events, observers and timestamps are triggered

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -13,21 +13,25 @@ class ColorSeeder extends Seeder
      */
     public function run(): void
     {
-        Color::insert([
-            ['name' => 'Black',          'hex_code' => '#000000'],
-            ['name' => 'White',          'hex_code' => '#FFFFFF'],
-            ['name' => 'Oak',            'hex_code' => '#D4A574'],
-            ['name' => 'Walnut',         'hex_code' => '#5C4033'],
-            ['name' => 'Grey',           'hex_code' => '#808080'],
-            ['name' => 'Beige',          'hex_code' => '#F5F5DC'],
-            ['name' => 'Charcoal',       'hex_code' => '#36454F'],
-            ['name' => 'Cream',          'hex_code' => '#FFFDD0'],
-            ['name' => 'Navy',           'hex_code' => '#1B2A4A'],
-            ['name' => 'Forest Green',   'hex_code' => '#2D5A27'],
-            ['name' => 'Terracotta',     'hex_code' => '#C0604A'],
-            ['name' => 'Sand',           'hex_code' => '#C2B280'],
-            ['name' => 'Brown',          'hex_code' => '#7B4F2E'],
-            ['name' => 'Dusty Rose',     'hex_code' => '#C2908A'],
-        ]);
+        $colors = [
+            'Black'        => '#000000',
+            'White'        => '#FFFFFF',
+            'Oak'          => '#D4A574',
+            'Walnut'       => '#5C4033',
+            'Grey'         => '#808080',
+            'Beige'        => '#F5F5DC',
+            'Charcoal'     => '#36454F',
+            'Cream'        => '#FFFDD0',
+            'Navy'         => '#1B2A4A',
+            'Forest Green' => '#2D5A27',
+            'Terracotta'   => '#C0604A',
+            'Sand'         => '#C2B280',
+            'Brown'        => '#7B4F2E',
+            'Dusty Rose'   => '#C2908A',
+        ];
+
+        foreach ($colors as $name => $hex) {
+            Color::create(['name' => $name, 'hex_code' => $hex]);
+        }
     }
 }
